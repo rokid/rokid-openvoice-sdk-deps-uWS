5348f8b130f79490a164efada840e1a2b87ddc96
ming <chen.zhang@rokid.com>
2017-12-25 19:55:44 +0800

    初始化uWebsockets代码
    基于v0.14.4
    
    Change-Id: Iddbd7023cb3f9798e7048132eddf377db768ea05

---
+++ Android.mk
@@ -0,0 +1,33 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libuWS
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := \
+	src/Epoll.cpp \
+	src/Extensions.cpp \
+	src/Group.cpp \
+	src/HTTPSocket.cpp \
+	src/Hub.cpp \
+	src/Networking.cpp \
+	src/Node.cpp \
+	src/Socket.cpp \
+	src/WebSocket.cpp
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/src \
+	external/zlib
+LOCAL_SHARED_LIBRARIES := libssl libcrypto libz
+LOCAL_CPPFLAGS := -std=c++11 -fexceptions
+ifeq ($(PLATFORM_SDK_VERSION), 23)
+LOCAL_CXX_STL := libc++
+LOCAL_C_INCLUDES += external/boringssl/include
+else ifeq ($(PLATFORM_SDK_VERSION), 22)
+LOCAL_SHARED_LIBRARIES += libc++ libdl
+LOCAL_C_INCLUDES += external/openssl/include
+else
+LOCAL_SDK_VERSION := 14
+LOCAL_NDK_STL_VARIANT := gnustl_static
+LOCAL_C_INCLUDES += external/openssl/include
+endif
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/src
+include $(BUILD_SHARED_LIBRARY)
